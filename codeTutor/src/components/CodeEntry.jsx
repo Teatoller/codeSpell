0d05f01b4fcce772be6e75af6f1e6f980786e1c9
Steven <steven.otieno1@gmail.com>
2025-10-23 08:55:10 +0300

    ft(header): add header component
    - render header

--- codeTutor/src/components/CodeEntry.jsx
+++ codeTutor/src/components/CodeEntry.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from './Header'
 
 const CodeEntry = () => {
   return (
-    <div>CodeEntry</div>
+    <div className='min-h-screen flex flex-col items-center p-6'><Header /></div>
   )
 }
 
